2278f7f02dcc77c0fa4bf1447bb9e72ed6f7dc55
Will Brown <brown5628@gmail.com>
2022-01-13 08:30:26 -0500

    Test CICD 1

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker! Test 1.'
+    return 'Hello, Docker! Test 1. Test 2.'
 
 if __name__ == '__main__':
     app.run(port=5000)
