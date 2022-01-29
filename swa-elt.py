c35164977b5c0509faf0236184b58ea8e341f7af
Will Brown <brown5628@gmail.com>
2022-01-29 16:53:58 -0500

    initial pipeline

--- swa-elt.py
+++ swa-elt.py
@@ -1,67 +1,46 @@
-from dagster import job, op, repository, schedule, ScheduleDefinition
+from dagster import job, op, repository, schedule, ScheduleDefinition, Nothing, In
 from dagster.utils.log import get_dagster_logger
 from dagster_gcp.gcs.io_manager import gcs_pickle_io_manager
 from dagster_gcp.gcs.resources import gcs_resource
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import re
+from dagster_gcp.bigquery.resources import bigquery_resource
+import dagster_gcp as gcp
+import json
 import pandas as pd
+import feedparser
+import datetime
 
 logger = get_dagster_logger()
+job_ts = datetime.datetime.utcnow()
 
-@op
-def generate_df_from_goodreads_soup():
-    """Extract a dataframe of the past 100 items in my public Goodreads RSS feed"""
-    page = "https://www.goodreads.com/review/list_rss/51384411?key=WtDvvUkHkxtfdlRlZRZ5v3AFe4OOq3SGxiQ0xn4NV5oDUDvJ&shelf=%23ALL%23"
-    html = urlopen(page)
-    soup = BeautifulSoup(html, "lxml")
-    filtered_soup = soup.find_all("description")
-    title_data = []
-    detail_data = []
-    # first row blank, 100 item feed
-    for item in range(1, 101):
-        row = filtered_soup[item]
+@op(
+    ins={
+        "start": In(Nothing)
+    }
+)
+def get_goodreads_feed(context):
+    goodreads_rss="https://www.goodreads.com/review/list_rss/51384411?key=WtDvvUkHkxtfdlRlZRZ5v3AFe4OOq3SGxiQ0xn4NV5oDUDvJ&shelf=%23ALL%23"
+    feeds=feedparser.parse(goodreads_rss)
 
-        title_tag = row.find(re.compile("img"))
-        title_text = title_tag.get("alt")
-        title_data.append(title_text)
+    post_ids = []
+    raw_data = []
+    pipeline_processed_at = []
 
-        row_data = row.text.strip()
-        row_data = row_data.split("\n")
-        row_data = [x.split(":", 1)[-1] for x in row_data]
-        row_data = [x.strip() for x in row_data]
-        detail_data.append(row_data)
+    for item in feeds.entries:
+        post_ids.append(item.id)
+        raw_data.append(json.dumps(item))
+        pipeline_processed_at.append(job_ts)
 
-    df_title = pd.DataFrame(columns=["title"], data=title_data)
-    df_detail = pd.DataFrame(
-        columns=[
-            "author",
-            "name",
-            "average_rating",
-            "book_published",
-            "rating",
-            "read_at",
-            "date_added",
-            "shelves",
-            "review",
-            "idk",
-            "idk2",
-            "idk3"
-        ],
-        data=detail_data,
-    )
-    df = pd.concat([df_title, df_detail], axis=1)
-    # drop final column since it is computer junk
-    df = df.iloc[:, :-1]
-    df = df.set_index('title')
-    logger.info(df.head())
+    df_ids = pd.DataFrame(columns=["id"], data=post_ids)
+    df_raw_data = pd.DataFrame(columns=["raw_data"], data=raw_data)
+    df_pipeline_processed_at = pd.DataFrame(columns=["_pipeline_processed_at"], data=pipeline_processed_at)
+    df = pd.concat([df_ids, df_raw_data, df_pipeline_processed_at], axis=1)
     return df
-
 
 @job(
     resource_defs={
         "gcs": gcs_resource,
         "io_manager": gcs_pickle_io_manager,
+        "bigquery": bigquery_resource,
     },
     config={
         "resources": {
@@ -70,12 +49,34 @@ def generate_df_from_goodreads_soup():
                     "gcs_bucket": "small-world-dagster",
                     "gcs_prefix": "dagster-logs",
                 }
+            },
+            "bigquery": {
+                "config": {
+                    "project": "small-world-analytics"
+                }
+            }
+        },
+        "ops": {
+            "bq_create_dataset": {
+                "config": {
+                    "dataset": "raw_goodreads",
+                    "exists_ok": True
+                }
+            },
+            "import_df_to_bq": {
+                "config": {
+                    "destination": "raw_goodreads.goodreads_feed",
+                    "load_job_config": {
+                        "write_disposition": "WRITE_TRUNCATE"
+                    }
+                }
             }
         }
     },
 )
 def batch_el():
-    generate_df_from_goodreads_soup()
+    df=get_goodreads_feed(start=gcp.bq_create_dataset())
+    gcp.import_df_to_bq(df=df)
 
 @schedule(
     cron_schedule="*/30 * * * *",
